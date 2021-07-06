95ac20ec948426ca2d66a801bb37a4705e74a8bc
Lorenzo978 <lorenzopoletti973@gmail.com>
2021-07-06 11:01:53 +0200

    Update platoon

--- scratch/platoon.cc
+++ scratch/platoon.cc
@@ -29,6 +29,7 @@
 #include "ns3/applications-module.h"
 #include "ns3/internet-module.h"
 #include "ns3/core-module.h"
+#include "string.h"
 
 NS_LOG_COMPONENT_DEFINE ("ThreeVehiclePlatoon");
 
@@ -37,56 +38,71 @@ using namespace millicar;
 
 uint32_t g_rxPackets; // total number of received packets
 uint32_t g_txPackets; // total number of transmitted packets
+uint32_t packetSentCounter;
+double lastAverageRTT;
+double averageRTT;
+
+
+uint32_t nodeNumber = 5;
+uint32_t packetSize = 1200; // UDP packet size in bytes
+uint32_t startTime = 1000; // application start time in milliseconds
+uint32_t endTime = 4300; // application end time in milliseconds
+double interPacketInterval = 33; // interpacket interval in milliseconds
+double intraGroupDistance = 5; // distance between two vehicles belonging to the same group
+const std::string channelCondition = "n";
+const std::string scenario = "V2V-Urban";
 
 Time g_firstReceived; // timestamp of the first time a packet is received
 Time g_lastReceived; // timestamp of the last received packet
 
 static void Rx (Ptr<OutputStreamWrapper> stream, Ptr<const Packet> p)
 {
- g_rxPackets++;
- SeqTsHeader header;
+  g_rxPackets++;
+  SeqTsHeader header;
 
- p->PeekHeader(header);
+  p->PeekHeader(header);
+  packetSentCounter++;
 
- *stream->GetStream () << Simulator::Now ().GetSeconds () << "\t" << p->GetSize() << "\t" << header.GetSeq() << "\t" << header.GetTs().GetSeconds() << std::endl;
+  *stream->GetStream () << Simulator::Now ().GetSeconds () << "\t" << packetSentCounter << "\t" << header.GetSeq() << "\t" << header.GetTs().GetSeconds() << std::endl;
+  
+  // Calcolo l'avarage RTT per l'ultimo veicolo del platoon
+  if(packetSentCounter == nodeNumber-1)
+  {
+    packetSentCounter = 0;
+    lastAverageRTT = lastAverageRTT + Simulator::Now ().GetSeconds ()-header.GetTs().GetSeconds();
+  }
 
- if (g_rxPackets > 1)
- {
+  
+  NS_LOG_DEBUG(Simulator::Now ().GetSeconds () << "\t" << header.GetSeq());
+  averageRTT = averageRTT + Simulator::Now ().GetSeconds ()-header.GetTs().GetSeconds();
 
-   g_lastReceived = Simulator::Now();
- }
- else
- {
-   g_firstReceived = Simulator::Now();
- }
+  if (g_rxPackets > 1)
+  {
+
+    g_lastReceived = Simulator::Now();
+  }
+  else
+  {
+    g_firstReceived = Simulator::Now();
+  }
 }
 
 int main (int argc, char *argv[])
 {
+  LogComponentEnable("ThreeVehiclePlatoon", LOG_LEVEL_DEBUG);
   std::cout << "----------- Start -----------" << std::endl;
-  // This script creates two nodes moving at 20 m/s, placed at a distance of 10 m.
-  // These nodes exchange packets through a UDP application,
-  // and they communicate using a wireless channel.
 
   // system parameters
   double bandwidth = 1e8; // bandwidth in Hz
   double frequency = 28e9; // the carrier frequency
   uint32_t numerology = 3; // the numerology
 
-  // applications
-  uint32_t nodeNumber = 6;
-  uint32_t packetSize = 350; // UDP packet size in bytes
-  uint32_t startTime = 1000; // application start time in milliseconds
-  uint32_t endTime = 4300; // application end time in milliseconds
-  uint32_t interPacketInterval = 33; // interpacket interval in milliseconds
-
   // mobility
   double speed = 27.7; // speed of the vehicles m/s
-  double intraGroupDistance = 1; // distance between two vehicles belonging to the same group
   uint32_t ct;
   double totalPacket = ((endTime-startTime)/interPacketInterval) * (nodeNumber-1);
   AsciiTraceHelper asciiTraceHelper;
-  Ptr<OutputStreamWrapper> stream = asciiTraceHelper.CreateFileStream ("scratch/lowest-6v-highdensity.txt");
+  Ptr<OutputStreamWrapper> stream = asciiTraceHelper.CreateFileStream ("scratch/test.txt");
 
   CommandLine cmd;
   //
@@ -101,7 +117,8 @@ int main (int argc, char *argv[])
   Config::SetDefault ("ns3::MmWavePhyMacCommon::CenterFreq", DoubleValue (frequency));
   Config::SetDefault ("ns3::MmWaveVehicularHelper::Bandwidth", DoubleValue (bandwidth));
   Config::SetDefault ("ns3::MmWaveVehicularHelper::Numerology", UintegerValue (numerology));
-  Config::SetDefault ("ns3::MmWaveVehicularPropagationLossModel::ChannelCondition", StringValue ("a"));
+  Config::SetDefault ("ns3::MmWaveVehicularPropagationLossModel::ChannelCondition", StringValue(channelCondition));
+  Config::SetDefault ("ns3::MmWaveVehicularPropagationLossModel::Scenario", StringValue(scenario));
   Config::SetDefault ("ns3::MmWaveVehicularPropagationLossModel::Shadowing", BooleanValue (true));
   Config::SetDefault ("ns3::MmWaveVehicularSpectrumPropagationLossModel::UpdatePeriod", TimeValue (MilliSeconds (1)));
   Config::SetDefault ("ns3::MmWaveVehicularAntennaArrayModel::AntennaElements", UintegerValue (16));
@@ -153,10 +170,6 @@ int main (int argc, char *argv[])
   Ptr<Ipv4StaticRouting> staticRouting = ipv4RoutingHelper.GetStaticRouting (n.Get (0)->GetObject<Ipv4> ());
   staticRouting->SetDefaultRoute (n.Get (1)->GetObject<Ipv4> ()->GetAddress (1, 0).GetLocal () , 2 );
 
-  NS_LOG_DEBUG("IPv4 Address node 0: " << n.Get (0)->GetObject<Ipv4> ()->GetAddress (1, 0).GetLocal ());
-  NS_LOG_DEBUG("IPv4 Address node 1: " << n.Get (1)->GetObject<Ipv4> ()->GetAddress (1, 0).GetLocal ());
-  NS_LOG_DEBUG("IPv4 Address node 2: " << n.Get (2)->GetObject<Ipv4> ()->GetAddress (1, 0).GetLocal ());
-
   Ptr<mmwave::MmWaveAmc> m_amc = CreateObject <mmwave::MmWaveAmc> (helper->GetConfigurationParameters());
 
   // setup the applications
@@ -183,17 +196,19 @@ int main (int argc, char *argv[])
     a.Stop (MilliSeconds (endTime));
   }
 
-  Simulator::Stop(MilliSeconds (endTime + 50));
+  Simulator::Stop(MilliSeconds (endTime + 100));
   Simulator::Run ();
   Simulator::Destroy ();
 
   std::cout << "----------- Statistics -----------" << std::endl;
+  std::cout << "Number of vehicles:\t" << nodeNumber << std::endl;
   std::cout << "Packets size:\t\t" << packetSize << " Bytes" << std::endl;
   std::cout << "Duration:\t\t" << endTime-startTime << std::endl;
   std::cout << "Packets received:\t" << g_rxPackets << std::endl;
   std::cout << "Total packet sent:\t" << totalPacket << std::endl;
-  std::cout << "Packet rate loss:\t" << ((totalPacket-g_rxPackets)/totalPacket) << "%" << std::endl;
-  std::cout << "Average Throughput:\t" << (double(g_rxPackets)*(double(packetSize)*8)/double( g_lastReceived.GetSeconds() - g_firstReceived.GetSeconds()))/1e6 << " Mbps" << std::endl;
-
+  std::cout << "Packet rate loss:\t" << ((totalPacket-g_rxPackets)/totalPacket)*100 << "%" << std::endl;
+  std::cout << "Average Throughput:\t" << (double(g_rxPackets)*(double(packetSize)*8)/double( g_lastReceived.GetSeconds() - g_firstReceived.GetSeconds()))/(1e6 * (nodeNumber-1)) << " Mbps" << std::endl;
+  std::cout << "Average RTT:\t\t" << averageRTT/g_rxPackets << std::endl;
+  std::cout << "Average last RTT:\t" << lastAverageRTT/(g_rxPackets/(nodeNumber-1)) << std::endl;
   return 0;
 }
